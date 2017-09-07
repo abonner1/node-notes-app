91a22e258db895382a994cbe1c034d2863560aa3
Andrew Bonner <andrewbonner92@gmail.com>
2017-09-07 18:39:13 -0400

    Done.

--- app.js
+++ app.js
@@ -1,16 +1,61 @@
-console.log("Starting app.js")
-
 const fs = require('fs')
-const os = require('os')
+const _ = require('lodash')
+const yargs = require('yargs')
+
 const notes = require('./notes')
 
-var result = notes.addNote('To be or not to be')
-console.log(result)
+var titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
 
-// var user = os.userInfo()
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//   if (err) {
-//     console.log('Unable to write to file')
-//   }
-// })
+var bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv
+var command = argv._[0]
+
+if (command === 'add') {
+  var note = notes.addNote(argv.title, argv.body)
+  if (note) {
+    console.log('Note created')
+    notes.logNote(note)
+  } else {
+    console.log("Note title taken.")
+  }
+} else if (command === 'list') {
+  var allNotes = notes.getAll()
+  console.log(`Printing ${allNotes.length} notes(s)...`)
+  allNotes.forEach((note) => notes.logNote(note))
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title)
+  if (note) {
+    console.log('Note found')
+    notes.logNote(note)
+  } else {
+    console.log('Note not found')
+  }
+} else if (command === 'remove') {
+  var isRemoved = notes.removeNote(argv.title)
+  var message = (isRemoved) ? 'Note was removed' : 'Note note found'
+  console.log(message)
+} else {
+  console.log('Command not recognized')
+}
